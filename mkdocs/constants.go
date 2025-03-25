9debd316e19fbb8674e42020603c20e80395d09f
Fred <fred@creativeprojects.tech>
2025-03-25 17:41:48 +0000

    upgrade theme version

--- mkdocs/constants.go
+++ mkdocs/constants.go
@@ -6,7 +6,7 @@ const (
 	sourceRepositoryPath = "./source"
 	themeRepositoryPath  = "./themes/hugo-theme-relearn"
 	sourceRepositoryURL  = "https://github.com/creativeprojects/resticprofile.git"
-	themeVersionTag      = "7.5.0"
+	themeVersionTag      = "7.6.0"
 	themeRepositoryOwner = "McShelby"
 	themeRepositoryName  = "hugo-theme-relearn"
 )
